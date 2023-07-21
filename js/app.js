e64a555f2de48c6c2acce44f454080267284f065
dariorod <dario2303@gmail.com>
2023-07-21 09:16:48 -0300

    insurance calculator proyect, init proyect

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function userInterfase() {}
+
+userInterfase.prototype.llenaOpciones = () => {
+    const max = new Date().getFullYear(), min = max - 20;
+    
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
